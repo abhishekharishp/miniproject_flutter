571a923a2109a4b5453316c1d1655f00a3086613
AB7zz <abhinavcv007@gmail.com>
2024-03-23 19:29:54 +0530

    phone and otp ui done

--- lib/screens/login/otp_screen.dart
+++ lib/screens/login/otp_screen.dart
@@ -8,8 +8,78 @@ class OTPScreen extends StatefulWidget {
 }
 
 class _OTPScreenState extends State<OTPScreen> {
+  String otp = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 350.0,
+          height: 400.0,
+          decoration: BoxDecoration(
+            color: const Color(0xFF233831),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'OTP',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(height: 40.0),
+              SizedBox(
+                width: 180.0,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'OTP',
+                      labelStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(),
+                      hintText: 'XXXXXX',
+                      hintStyle: TextStyle(color: Colors.grey)),
+                  keyboardType: TextInputType.phone,
+                  style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    setState(() {
+                      otp = value;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 40.0),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 56.0,
+                    height: 56.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFEDE7DE), // Circular background color
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Handle button press
+                      Navigator.pushNamed(context, '/login/input');
+                    },
+                    icon: const Icon(
+                      Icons.arrow_forward,
+                      color: Color(0xFF4C6763),
+                      size: 32.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/login/phone_screen.dart
+++ lib/screens/login/phone_screen.dart
@@ -8,8 +8,78 @@ class PhoneScreen extends StatefulWidget {
 }
 
 class _PhoneScreenState extends State<PhoneScreen> {
+  String phoneNumber = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 350.0,
+          height: 400.0,
+          decoration: BoxDecoration(
+            color: const Color(0xFF233831),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Enter your mobile no.',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                ),
+              ),
+              const SizedBox(height: 40.0),
+              SizedBox(
+                width: 180.0,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      labelText: 'Phone Number',
+                      labelStyle: TextStyle(color: Colors.white),
+                      border: OutlineInputBorder(),
+                      hintText: '9778393558',
+                      hintStyle: TextStyle(color: Colors.grey)),
+                  keyboardType: TextInputType.phone,
+                  style: const TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                  onChanged: (value) {
+                    setState(() {
+                      phoneNumber = value;
+                    });
+                  },
+                ),
+              ),
+              const SizedBox(height: 40.0),
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 56.0,
+                    height: 56.0,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFFEDE7DE), // Circular background color
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Handle button press
+                      Navigator.pushNamed(context, '/login/otp');
+                    },
+                    icon: const Icon(
+                      Icons.arrow_forward,
+                      color: Color(0xFF4C6763),
+                      size: 32.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
